ba8019b4209b3dd3b8f8a9a64510380b094bbb86
hernandezperezedgargabrielg23-cpu <hernandezperezedgargabriel.g23@cecyt12.edu.mx>
2025-10-20 17:46:19 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const banner = document.getElementById('cookie-banner');
-const acceptBtn = document.getElementById('accept-cookies');
-const rejectBtn = document.getElementById('reject-cookies');
-if (localStorage.getItem('cookieConsent')) { banner.style.display = 'none'; }
-acceptBtn.addEventListener('click', () => { localStorage.setItem('cookieConsent', 'accepted'); banner.style.display = 'none'; });
-rejectBtn.addEventListener('click', () => { localStorage.setItem('cookieConsent', 'rejected'); banner.style.display = 'none'; });
